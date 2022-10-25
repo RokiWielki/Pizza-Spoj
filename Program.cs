f031e06048079ef9dd5cbe14f107559585347c13
Roki <Roki@DESKTOP-3PRGG7M>
2022-10-25 23:50:01 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Pizza
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int T=int.Parse(Console.ReadLine());
+            int[] N = new int[T];
+            for (int i = 0; i < T; i++)
+            {
+                N[i] = int.Parse(Console.ReadLine());
+                
+            }
+            for (int i = 0; i < T; i++)
+            {
+                N[i] *= 2;
+                Console.WriteLine(N[i]);
+            }
+        }
+
+    }
+}
