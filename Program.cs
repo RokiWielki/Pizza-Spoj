49586438fec3dcba45ad84734050e15dc7419170
RokiWielki <Cruck222@interia.pl>
2022-11-08 01:10:46 +0100

    Version 4

--- Program.cs
+++ Program.cs
@@ -1,32 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text;
 
 
 namespace Pizza
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
-            int T=int.Parse(Console.ReadLine());
-            
-            if (1<=T && T<=1000000)
-            {
-                int[] N = new int[T];
-                for (int i = 0; i < T; i++)
-                {
-                    N[i] = int.Parse(Console.ReadLine());
+            var answer = new StringBuilder();
+            int T = int.Parse(Console.ReadLine());
 
-                }
-                for (int i = 0; i < T; i++)
-                {
-                    N[i] *= T;
-                    Console.WriteLine(N[i]);
-                }
+            for (int i = 1; i <= T; i++)
+            {
+                long N = long.Parse(Console.ReadLine());
+                answer.AppendLine(Recurence(N).ToString());
             }
-            
+            Console.WriteLine(answer.ToString());
+        }
+
+    
+        static long Recurence(long N)
+        {
+            if (N == 1)
+                return 2;
+            return (N * N + N + 2) / 2;
         }
 
     }
+
+    
 }
